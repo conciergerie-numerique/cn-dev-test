9d31726e0561eba11077fa48986dadee4e82d061
Conciergerie Numérique <contact@conciergerie-numerique.fr>
2019-11-04 19:44:37 +0100

    add fixtures and vue.js tools

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 }
 
 Encore
-    // directory where compiled assets will be stored
+// directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
@@ -48,7 +48,8 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     // enables @babel/preset-env polyfills
-    .configureBabel(() => {}, {
+    .configureBabel(() => {
+    }, {
         useBuiltIns: 'usage',
         corejs: 3
     })
@@ -71,6 +72,20 @@ Encore
     //.addEntry('admin', './assets/js/admin.js')
 
     .enableVueLoader()
+
+    // @see https://symfony.com/doc/current/frontend/encore/copy-files.html
+    .copyFiles(
+        {
+            from: './assets/images',
+
+            // optional target path, relative to the output dir
+            // if versioning is enabled, add the file hash too
+            to: 'images/[path][name].[hash:8].[ext]',
+
+            // only copy files matching this pattern
+            // pattern: /\.(png|jpg|jpeg)$/
+        }
+    )
 ;
 
 module.exports = Encore.getWebpackConfig();
